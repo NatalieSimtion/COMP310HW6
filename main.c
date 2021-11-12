134ecebd2c190afb74757bf40b0560a8662516de
Natalie Simtion <74635514+NatalieSimtion@users.noreply.github.com>
2021-11-12 02:49:07 +0000

    node,edges,header,src and dest

--- main.c
+++ main.c
@@ -3,9 +3,58 @@
 
 #define N 8 //vertices
 
-//data structure
+//data structure 4 graph
 
 
-int main(void) {
+struct GraphStructure {
+  //array of pointers to Node
+struct Node* head[N];
+
+};
+
+
+//data structure 4 nodes of graph
+struct Node {
+  int destination;
+  struct Node* next;
+};
+
+//data struct 4 edge
+
+struct Edge {
+  int source, destination;
+};
+
+//adjacency list 4  from edges
+struct GraphStructure* createGraph(struct Edge edges[], int n)  {
+
+  //storage
+
+    struct GraphStructure* graph = (struct Graph*)malloc(sizeof(struct GraphStructure));
+
+    //head pointer 4 vertex components
+
+    for(int i = 0;  i < N; i++) {
+      graph->head[i] = NULL;
+    }
+    for(int i = 0; i < n; i++) {
+      //source and destination vertex
+      int source = edges[i].source;
+      int destination = edges[i].destination;
+
+      //new node from source to destintation
+      struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+      newNode->destination = destination;
+
+      //new node to current head
+      newNode->next = graph->head[source];
+
+      //header  to new node
+      graph->head[source] = newNode;
+
+    }
+    return graph;
+
 
 }
+
