547a9897070b926bea37a4eddd3943f6ebbf7f9d
Natalie Simtion <74635514+NatalieSimtion@users.noreply.github.com>
2021-11-12 05:13:44 +0000

    main

--- main.c
+++ main.c
@@ -74,5 +74,24 @@ void printGraph( struct GraphStructure* graph) {
   }
 }
 
+int main(void) {
+  struct Edge edges[] = {
+    {1,2},{1,3},{1,7},{2,4},{3,4},{4,5},{7,6},{6,5},{6,8}
+  };
+  //edges
+  int nEdges = sizeof(edges)/sizeof(edges[0]);
 
+  struct GraphStructure *graph = createGraph(edges,nEdges);
 
+  printGraph(graph);
+
+  return 0;
+}
+//output is below
+//(1 -> 7)    (1 -> 3)    (1 -> 2)
+//(2 -> 4)
+//(3 -> 4)
+//(4 -> 5)
+
+//(6 -> 8)    (6 -> 5)/
+//(7 -> 6)
